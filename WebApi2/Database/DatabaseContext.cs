96da0493cd262046650ca61311319b0141fb4d96
kasi <kasinayana2@gmail.com>
2022-12-01 11:24:35 +0530

    Add project files.

---
+++ WebApi2/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApi2.Models;
+
+namespace WebApi2.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection")
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApi2/Migrations/202211301237277_initial.cs
@@ -0,0 +1,28 @@
+namespace WebApi2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Address = c.String(),
+                        Contact = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
