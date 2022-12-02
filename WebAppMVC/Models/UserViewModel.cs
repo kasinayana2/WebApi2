302c0304d7f059c3b72a984ee23ef75d88b83642
kasi <kasinayana2@gmail.com>
2022-12-02 11:17:53 +0530

    i created one new mvc project then consum webapi to mvc

---
+++ WebAppMVC/Models/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Contact { get; set; }
+
+    }
+}
